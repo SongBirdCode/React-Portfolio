9fa9f3417b9795b4f78567bd86ddf50373585b1d
SongBirdCode <Songbirdcode@gmail.com>
2024-06-13 15:43:58 -0400

    Updated Portfolio

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// Recent Photo or Avatar
+// Short Biography
+import React from "react";
 
-function App() {
+function About() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="content is-medium">Welcome to SongBirdCode: An eclectic portfolio created by Full Stack Software Engineer, Gina Bauza.</p>
+      <hr />
+      <img className="my-pic" src={process.env.PUBLIC_URL + '/img/profile.jpg'} alt="Gina Bauza Profile"/>
+      <p className="content is-italic mt-4">
+        
+      </p>
+      <p className="content">
+        Hello there! Welcome to my portfolio. From Air Force Veteran, Opera crossover professional singer, and software engineer there is no design too scary or application too rocky to handle. Gina has lived a life full of many travels and languages, so of course computer languages hold importance. Thanks for stopping by! (Tweet Tweet!)
+      </p>
     </div>
   );
 }
 
-export default App;
+export default About;
