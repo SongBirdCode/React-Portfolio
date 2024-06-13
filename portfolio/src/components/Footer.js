7412c68393ad18dfd44288bbabd3991717fae3ab
SongBirdCode <Songbirdcode@gmail.com>
2024-06-13 15:53:55 -0400

    updated js files within components

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/SongBirdCode" target="_blank" rel="noreferrer">
+            SongBirdCode: Gina Bauza
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com//"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>{" "}
+          |{" "}{" "}
+          <a href="" target="_blank" rel="noreferrer">
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
